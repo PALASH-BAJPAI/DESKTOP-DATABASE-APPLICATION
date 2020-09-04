69744f7417a109bd7bcff19ae9903581aebe43aa
PALASH <63140586+PALASH-BAJPAI@users.noreply.github.com>
2020-09-04 08:12:42 +0530

    GUI app for bookstore completed

---
+++ PROJECT/Backend.py
@@ -0,0 +1,50 @@
+import sqlite3
+
+def connect():
+    conn=sqlite3.connect("books.db")            #database name is book
+    cur=conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS book(id INTEGER PRIMARY KEY, title text, author text, year integer,isbn integer)")
+    conn.commit()
+    conn.close()
+
+
+def insert(title,author,year,isbn):
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("INSERT INTO book VALUES (NULL,?,?,?,?)",(title,author,year,isbn))
+    conn.commit()
+    conn.close()
+
+def view():
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("SELECT * FROM book")
+    rows=cur.fetchall()
+    conn.close()
+    return rows
+
+def search(title="",author="",year="",isbn=""):     #provide an active string so if given value not passed then also fn runs
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("SELECT * FROM book WHERE title=? OR author=? OR year=? OR isbn=?",(title,author,year,isbn))
+    rows=cur.fetchall()
+    conn.close()
+    return rows
+
+def delete(id):
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("DELETE FROM book WHERE id=?",(id,))        #(don't forget comma , after id)
+    conn.commit()
+    conn.close()
+
+def update(id,title,author,year,isbn):
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    delete(id)
+    insert(title,author,year,isbn)
+    conn.commit()
+    conn.close()
+
+connect()
+
